839c81d6ce20bd371c720562a2b013a00e974102
Pacix9 <borsatogustavo591@gmail.com>
2024-11-14 15:03:10 -0300

    DT5
    desafio tecnico 5

---
+++ DT5_class (1).cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Personagem
+    {
+        int kiMedio;
+        string nome;
+        bool ssj;
+
+        public Personagem(int kiMedio, string nome, bool ssj)
+        {
+            this.nome = nome;
+            this.kiMedio = kiMedio;
+            this.ssj = ssj;
+        }
+       
+        
+
+
+    }
+}
---
+++ DT5_main (2).cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem P1 = new Personagem(150000000, "Goku", true );
+            Personagem P2 = new Personagem(145000000, "Vegeta", true);
+           
+
+            
+            
+            
+        }
+    }
+}
